3226555a9149f2da3d9532d9ca80edcc7d8f4e2d
HimanshiAhir <himanshiahir770@gmail.com>
2023-08-17 20:34:17 +0530

    Uploaded Project

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const POPUP_KEY = '7ffb18f7-a4a5-45ae-aa6a-c68204325f2f';
